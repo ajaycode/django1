d0e84f15d8eb1493e1d544c11e13c5bf05a0c3e8
Ajay <writeajay@gmail.com>
2016-02-19 17:43:53 +0530

    removed some imports

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django import template
 from django.template.loader import get_template
-from django.template import Context
+
 
 
 # Create your views here.
