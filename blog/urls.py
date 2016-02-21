f936f2a6a363cd70c7acec45419a943ba55e77ec
Ajay <writeajay@gmail.com>
2016-02-21 12:54:09 +0530

    Last two checkins do not work.  Info does not show up in server/people_list.html

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,15 @@
 __author__ = 'Ajay'
-from django.conf.urls import url
+from django.conf.urls import url, patterns, include
 from . import views
+from blog.views import Index, PeopleList
+#from django.contrib import admin
+#admin.autodiscover()
 
 urlpatterns = [
     #url(r'^$', views.post_list, name='post_list'),
     url(r'^hello$', views.hello, name='post_list'),
+    url (r'^$', Index.as_view()),
+    url (r'^people$', PeopleList.as_view()),
+    url (r'^people_list$', views.people_list)
+    #url (r'^admin/', include (admin.site.urls))
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django import template
 from django.template.loader import get_template
-
+from django.views.generic import View
+from blog.models import Person
 
 
 # Create your views here.
@@ -15,3 +16,23 @@ def hello (request):
     t = get_template('base.html')
     html = t.render({'name': "Jane"})
     return HttpResponse (html)
+
+class Index (View):
+    def get (self, request):
+        return  HttpResponse ("Called from a get Request")
+    def post (self, request):
+        return  HttpResponse ('Called from a post request')
+
+class PeopleList (View):
+    def get(self, request):
+        people = Person.objects.all()
+        pers = people[0]
+        return render ('profile.html', pers)
+
+def people_list (request):
+    if request.method == "GET":
+
+        #persons = {{'first_name': "Yudhishtira", 'last_name': 'Pandava'}, {'first_name': "Nakul", 'last_name': 'Pandava'}}
+        person = {'first_name': "Yudhishtir", 'last_name': 'Pandava'}
+        return render(request,'profile.html' , {'person': person})
+        #return HttpResponse (html)
